3b2ffab384eb27fc254b351a4617e739ec7dbc31
DaveJosef <josedaaaaavid@gmail.com>
2023-08-21 18:22:31 -0300

    Fixing Non-relative paths in Typescript config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '@/styles/Home.module.css';
-import TaskForm from '@/Components/TaskForm/TaskForm';
-import TaskList from '@/Components/TaskList/TaskList';
 import Main from './Main';
 
 export default function Home() {
